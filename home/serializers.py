96a5945b4f31196628b647ea8ae21073c22f5fe8
ahmed.farjana93 <ahmed.farjana93@gmail.com>
2025-07-16 01:29:33 +0200

    feat(validation): add custom checks for todo_title field in serializer

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
 from .models import Todo
+import re
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = '__all__'  # includes all model fields: uid, title, etc.
+        fields = ['uid', 'todo_title', 'todo_description', 'is_done'] # includes some necessary model fields: title, description etc that is required by client or developer.
+#       exclude = ['created_at'] #when you have more fields supose 100 and one field don't want to show but others want to show than have to use exclude just to write which fields you don't want to show.
+#       fields = '__all__'  # includes all model fields: uid, title, etc.
+
+    def validate_todo_title(self, data):
+        # Check if value exists (can skip this; DRF already handles required=True)
+        if data:
+            # Length check
+            if len(data) <= 3:
+                raise serializers.ValidationError("todo_title must be longer than 3 characters.")
+
+            # Special character check
+            regex = re.compile(r'[@_!#$%^&*()<>?/\|}{~:]')
+            if regex.search(data):
+                raise serializers.ValidationError("todo_title cannot contain special characters.")
+
+        return data
