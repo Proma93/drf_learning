305bba8a81df275d8e51ec794e297668b15edc4b
ahmed.farjana93 <ahmed.farjana93@gmail.com>
2025-07-16 01:28:13 +0200

    Add URL pattern for get_todo endpoint

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('post-todo/', post_todo, name='post_todo'),
+    path('get-todo/', get_todo, name='get_todo'),
+    path('post-todo/', post_todo, name='post_todo')
 ]
