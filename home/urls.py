9dcd03323b11944e9d8d118002cfd16cf12a3d1b
ahmed.farjana93 <ahmed.farjana93@gmail.com>
2025-07-16 23:49:13 +0200

    feat(routing): add URL path for patch_todo API

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('get-todo/', get_todo, name='get_todo'),
-    path('post-todo/', post_todo, name='post_todo')
+    path('post-todo/', post_todo, name='post_todo'),
+    path('patch-todo/', patch_todo, name='patch_todo'),
 ]
