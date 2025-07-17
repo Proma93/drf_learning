351d0bb9e507727be8b741f22b16848ec9457fa3
ahmed.farjana93 <ahmed.farjana93@gmail.com>
2025-07-17 02:26:09 +0200

    refactor(routing): switch URL patterns to use class-based views for Todo endpoints

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('get-todo/', get_todo, name='get_todo'),
-    path('post-todo/', post_todo, name='post_todo'),
-    path('patch-todo/<uuid:uid>/', patch_todo, name='patch_todo'),
+#    path('', home, name='home'),
+#    path('get-todo/', get_todo, name='get_todo'),
+#    path('post-todo/', post_todo, name='post_todo'),
+#    path('patch-todo/<uuid:uid>/', patch_todo, name='patch_todo'),
+
+    path('', HomeView.as_view(), name='home'),
+    path('todos/', TodoListCreateView.as_view(), name='todo-list-create'),
+    path('todos/<uuid:uid>/', TodoUpdateView.as_view(), name='todo-update'),
 ]
