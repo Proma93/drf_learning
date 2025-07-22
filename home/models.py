e3787f35edd75061a49ece65864fcb797a57a7ec
ahmed.farjana93 <ahmed.farjana93@gmail.com>
2025-07-23 00:25:31 +0200

    feat: add session_key field and customize __str__ method in Todo model

--- home/models.py
+++ home/models.py
@@ -13,9 +13,13 @@ class BaseModel(models.Model):
 
 class Todo(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True, related_name='todos')
+    session_key = models.CharField(max_length=40, null=True, blank=True)
     todo_title = models.CharField(max_length=100)
     todo_description = models.TextField()
     is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.todo_title} - {self.user or self.session_key}"   
 
 class TimingTodo(BaseModel):
     todo = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name='timingtodos')
