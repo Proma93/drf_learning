7635f158ded9903dca020440bfece60179467df6
ahmed.farjana93 <ahmed.farjana93@gmail.com>
2025-07-23 00:29:17 +0200

    feat: add owner validation in TimingTodoSerializer to restrict todo access

--- home/serializers.py
+++ home/serializers.py
@@ -7,7 +7,19 @@ import re
 class TimingTodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = TimingTodo
-        exclude = ['created_at', 'updated_at']
+        fields = '__all__'
+
+    def validate_todo(self, value):
+        request = self.context['request']
+        if request.user.is_authenticated:
+            if value.user != request.user:
+                raise serializers.ValidationError("You do not own this todo.")
+        else:
+            session_key = request.session.session_key or request.session.save()
+            if value.session_key != request.session.session_key:
+                raise serializers.ValidationError("You do not own this todo.")
+        return value
+        
 
 class TodoSerializer(serializers.ModelSerializer):  
     timingtodos = TimingTodoSerializer(many=True, read_only=True)
@@ -16,6 +28,7 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ['user', 'uid', 'todo_title', 'slug', 'todo_description', 'is_done', 'timingtodos'] # includes some necessary model fields: title, description etc that is required by client or developer.
+        read_only_fields = ['user', 'session_key']
 #       exclude = ['created_at'] #when you have more fields supose 100 and one field don't want to show but others want to show than have to use exclude just to write which fields you don't want to show.
 #       fields = '__all__'  # includes all model fields: uid, title, etc.
 
