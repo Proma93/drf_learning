9f5c907fe09e505eb545e48cb688b3cc43cf4174
ahmed.farjana93 <ahmed.farjana93@gmail.com>
2025-07-23 16:08:55 +0200

    feat: integrate Swagger UI for API documentation using drf_yasg

--- drfproject/urls.py
+++ drfproject/urls.py
@@ -19,8 +19,28 @@ from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
 from django.contrib import admin
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Task Tracker API",
+        default_version='v1',
+        description="API for managing Todos and TimingTodos",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('token/', obtain_auth_token, name='token'),
     path('', include('home.urls')),   # This includes the home app's URLs
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
